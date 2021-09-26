36b48f36eb3cc9b75296ef22ac76f3fd5997154e
nato-re <alvares.renato@hotmail.com>
2021-09-25 22:17:46 -0300

    router working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { HashRouter, Link, Route, Switch } from 'react-router-dom'
+import Xablau from './Xablau';
 function App() {
+  console.log(process.env);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Switch>
+        <Route exact path='/'>
+          <Link to='/xablau'> 
+          To xablau
+          </Link>
+          <img src={logo} />
+        </Route>
+        <Route exact path='/xablau'>
+          <Xablau />
+        </Route>
+      </Switch>
+    </HashRouter>
   );
 }
 
---
+++ src/Xablau.js
@@ -0,0 +1,6 @@
+export default function Xablau () {
+    console.log(process.env);
+    return (
+        <h1>Xablox</h1>
+    )
+} 
